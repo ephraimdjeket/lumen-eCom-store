52c3fe18f6f564b75c6ad4f7be4a75e31121f54e
Ephraim Djeket <ephdje50146@stud.noroff.no>
2025-08-20 13:32:32 +0200

    Changed importing name of home module css file

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,17 +1,51 @@
-import classes from "./Home.module.css";
+import h from "./Home.module.css";
 
 export default function Home() {
   return (
-    <section className={classes.homeContainer}>
-      <div className={classes.test1}>
-        <div className={classes.product}>
-          <div>          <h2>Lumen eCom Store</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae facilis sapiente, itaque cum officia veritatis atque voluptatibus repellendus in quas reprehenderit laudantium exercitationem quo omnis quasi expedita illum soluta tenetur.</p></div>
+    <>
+      <section className={h.homeContainer}>
+        <div className={h.herocard}>
+          <div className={h.herocontent}>
+            <div>
+              <h2>Find your next favorite product</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae facilis sapiente, itaque cum officia veritatis atque voluptatibus repellendus in quas reprehenderit laudantium exercitationem quo omnis quasi expedita illum soluta tenetur.</p></div>
+          </div>
+          <a href="#products"> Browse all</a>
         </div>
-      </div>
-      <div className={classes.test2}></div>
-      <div className={classes.test3}></div>
-      <div className={classes.test4}></div>
-    </section>
+        <article className={`${h.card} ${h.firstcard}`}>
+          {/* <img src="" alt="" /> */}
+          <div className={h.imagedummy}></div>
+          <h2>Product name</h2>
+          <p>$999</p>
+          <button className={h.button}>View Product</button>
+        </article>
+        <article className={`${h.card} ${h.secondcard}`}>
+          {/* <img src="" alt="" /> */}
+          <div className={h.imagedummy}></div>
+          <h2>Product name</h2>
+          <p>$999</p>
+          <button className={h.button}>View Product</button>
+        </article>
+        <article className={`${h.card} ${h.thirdcard}`}>
+          {/* <img src="" alt="" /> */}
+          <div className={h.imagedummy}></div>
+          <h2>Product name</h2>
+          <p>$999</p>
+          <button className={h.button}>View Product</button>
+        </article>
+        <section className={h.products} id="products">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </section>
+      </section >
+    </>
   )
 }
