74f832553812b532708b953c1c47ae1916846381
Ephraim Djeket <ephdje50146@stud.noroff.no>
2025-08-21 12:17:17 +0200

    Adjusted footer and implemented updated icons

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,16 +1,18 @@
-import classes from "./Footer.module.css";
+import f from "./Footer.module.css";
+import { SiFacebook, SiInstagram, SiX } from "react-icons/si";
 
 export default function Footer() {
   return (
-    <footer className={classes.footer}>
-      <div className={classes.footerContent}>
-        <ul>
-          <li>Home</li>
-          <li>Contact</li>
-          <li>Terms of Services</li>
+    <footer className={f.footer}>
+      <div className={f.footerContent}>
+        <ul className={f.list}>
+          <li><a href="#">Terms of Services</a></li>
+          <li><a href="#"><SiFacebook size={20} /></a></li>
+          <li><a href="#"><SiInstagram size={20} /></a></li>
+          <li><a href="#"><SiX size={20} /></a></li>
+
         </ul>
-        <p>(c) 2025 Ephraim Djeket
-        </p>
+        <p>© 2025 Ephraim Djeket</p>
       </div>
     </footer>
   )
