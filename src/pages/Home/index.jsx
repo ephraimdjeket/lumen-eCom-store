b502f0cf73c1d6465624b54ff26ff60573e06096
Ephraim Djeket <ephdje50146@stud.noroff.no>
2025-09-02 22:05:33 +0200

    Fetching API products and rendering on page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,26 @@
 import h from "./Home.module.css";
 import Card from "../../components/Card";
+import { useState, useEffect } from "react";
 
 
 export default function Home() {
+  const url = "https://v2.api.noroff.dev/online-shop";
+
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    async function productData() {
+      const res = await fetch(url);
+      if (!res.ok) throw new Error("Failed to fetch");
+      const { data } = await res.json();
+      setProducts(data);
+    }
+    productData();
+  }, [])
+
+
   return (
     <>
       <section className={h.homeContainer}>
@@ -12,16 +30,13 @@ export default function Home() {
               <h2>Find your next favorite product</h2>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae facilis sapiente, itaque cum officia veritatis atque</p></div>
             <input aria-label="search input" className={h.searchinput} id="search" type="search" placeholder="Search products by title..." />
-            <a href="#products" class={`${h.button} ${h.browseall}`}> Browse all</a>
+            <a href="#products" className={`${h.button} ${h.browseall}`}> Browse all</a>
           </div>
         </div>
         <section className={h.cardswrapper} id="products">
-          < Card />
-          < Card />
-          < Card />
-          < Card />
-          < Card />
-          < Card />
+          {products.map((product) => (
+            <Card key={product.id} {...product} />
+          ))}
         </section>
       </section >
     </>
