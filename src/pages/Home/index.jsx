3f810ee0aa9d6f7465cdb29dec1cc702bd3660ce
Ephraim Djeket <ephdje50146@stud.noroff.no>
2025-09-03 21:32:40 +0200

    Added comment

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -9,6 +9,7 @@ export default function Home() {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
+  /*Add error + loading*/
 
   useEffect(() => {
     async function productData() {
