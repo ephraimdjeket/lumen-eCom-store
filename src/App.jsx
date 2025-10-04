fad2d9e9d6bcbfbc5607481afc934de8a4ca4dd8
Ephraim Djeket <ephdje50146@stud.noroff.no>
2025-10-04 07:51:51 +0200

    Added Route, Routes and Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import Header from "./components/Header";
 import Home from "./pages/Home";
-import Footer from "./components/Footer";
+import Contact from "./pages/Contact";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/utils/Layout";
+import NotFound from "./components/utils/NotFound";
 
 export default function App() {
   return (
     <>
-      <Header />
-      <Home />
-      <Footer />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={< Home />} />
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
     </>
   )
 }
