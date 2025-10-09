584fea2a356b1225ebb2391e2937335f9fb81e36
Ephraim Djeket <ephdje50146@stud.noroff.no>
2025-10-09 20:16:33 +0200

    Controlled form component

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -1,22 +1,79 @@
 import c from "./Contact.module.css";
+import { useState } from "react";
 
 export default function Contact() {
+  const [fullName, setFullName] = useState("");
+  const [subject, setSubject] = useState("");
+  const [email, setEmail] = useState("");
+  const [userMessage, setUserMessage] = useState("");
+
+
+
+  function onTextInputChange(e) {
+    const { name, value } = e.target;
+
+    if ("fullName" === name) {
+      setFullName(value);
+    } if ("subject" === name) {
+      setSubject(value);
+    } if ("email" === name) {
+      setEmail(value);
+    } if ("userMessage" === name) {
+      setUserMessage(value);
+    }
+
+    const body = {
+      fullName,
+      subject,
+      email,
+      userMessage,
+    }
+    console.log(body)
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+  }
+
   return (
     <section className={c.homeContainer}>
       <div className={c.contactForm}>
         <h1>Contact us</h1>
         <form onSubmit={(e) => handleSubmit(e)}>
-          <div className={c.inputGroup}>          <label htmlFor="fullName">Full Name:</label>
-            <input type="text" id="fullName" />
+          <div className={c.inputGroup}>
+            <label htmlFor="fullName">Full Name:</label>
+            <input
+              type="text"
+              name="fullName"
+              value={fullName}
+              onChange={onTextInputChange}
+              required />
           </div>
-          <div className={c.inputGroup}>          <label htmlFor="subject">Subject:</label>
-            <input type="text" id="subject" />
+          <div className={c.inputGroup}>
+            <label htmlFor="subject">Subject:</label>
+            <input
+              type="text"
+              name="subject"
+              value={subject}
+              onChange={onTextInputChange}
+              required />
           </div>
-          <div className={c.inputGroup}>          <label htmlFor="email">Email:</label>
-            <input type="email" id="email" />
+          <div className={c.inputGroup}>
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              name="email"
+              value={email}
+              onChange={onTextInputChange}
+              required />
           </div>
-          <div className={c.inputGroup}>          <label htmlFor="body">Body:</label>
-            <textarea id="body" />
+          <div className={c.inputGroup}>
+            <label htmlFor="body">Body:</label>
+            <textarea
+              name="userMessage"
+              value={userMessage}
+              onChange={onTextInputChange}
+              required />
           </div>
           <input className={c.submitFormBtn} type="submit" />
 
