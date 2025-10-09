cfd808880ecdcaf4d1f2da18101cacb9c75ad590
Ephraim Djeket <ephdje50146@stud.noroff.no>
2025-10-09 20:16:56 +0200

    Worked on shoppingcart UI

--- src/pages/Shoppingcart/index.jsx
+++ src/pages/Shoppingcart/index.jsx
@@ -4,9 +4,8 @@ export default function ShoppingCart() {
   return (
     <section className={s.cartwrapper}>
       <div className={s.cartcontainer}>
-
         <div className={s.cartitem}>
-          <img src="https://www.shutterstock.com/shutterstock/photos/2286554497/display_1500/stock-photo-random-pictures-cute-and-funny-2286554497.jpg" alt="potato" />
+          <img src="https://blocks.astratic.com/img/general-img-landscape.png" alt="image placeholder" />
           <div className={s.cartinfo}>
             <h2>Item name</h2>
             <span className={s.line}></span>
@@ -16,14 +15,93 @@ export default function ShoppingCart() {
               <button className={s.quantitybutton}>-</button><span>1</span><button className={s.quantitybutton}>+</button>
             </div>
             <p>Subtotal:</p>
+            <button className={s.removeproduct}>Remove product</button>
+          </div>
+          <div className={s.ordersummary}>
+            <h2>Order Summary</h2>
+            <span className={s.line}></span>
+            <p>Cart total: $87</p>
+            <div className={s.cartoptionbuttons}>
+              <button className={s.proceedbtn}>Proceed to checkout</button>
+              <button className={s.clearcartbtn}>Clear cart</button>
+            </div>
           </div>
         </div>
-        <div className={s.ordersummary}>
-          <h2>Order Summary</h2>
-          <p>Cart total: $87</p>
+        <div className={s.cartitem}>
+          <img src="https://blocks.astratic.com/img/general-img-landscape.png" alt="image placeholder" />
+          <div className={s.cartinfo}>
+            <h2>Item name</h2>
+            <span className={s.line}></span>
+            <p>Price: $434</p>
+            <p>Discount price here</p>
+            <div className={s.quantitydisplay}>
+              <button className={s.quantitybutton}>-</button><span>1</span><button className={s.quantitybutton}>+</button>
+            </div>
+            <p>Subtotal:</p>
+            <button className={s.removeproduct}>Remove product</button>
+          </div>
+          <div className={s.ordersummary}>
+            <h2>Order Summary</h2>
+            <span className={s.line}></span>
+            <p>Cart total: $87</p>
+            <div className={s.cartoptionbuttons}>
+              <button className={s.proceedbtn}>Proceed to checkout</button>
+              <button className={s.clearcartbtn}>Clear cart</button>
+            </div>
+          </div>
+        </div>
+        <div className={s.cartitem}>
+          <img src="https://blocks.astratic.com/img/general-img-landscape.png" alt="image placeholder" />
+          <div className={s.cartinfo}>
+            <h2>Item name</h2>
+            <span className={s.line}></span>
+            <p>Price: $434</p>
+            <p>Discount price here</p>
+            <div className={s.quantitydisplay}>
+              <button className={s.quantitybutton}>-</button><span>1</span><button className={s.quantitybutton}>+</button>
+            </div>
+            <p>Subtotal:</p>
+            <button className={s.removeproduct}>Remove product</button>
+          </div>
+          <div className={s.ordersummary}>
+            <h2>Order Summary</h2>
+            <span className={s.line}></span>
+            <p>Cart total: $87</p>
+            <div className={s.cartoptionbuttons}>
+              <button className={s.proceedbtn}>Proceed to checkout</button>
+              <button className={s.clearcartbtn}>Clear cart</button>
+            </div>
+          </div>
+        </div>
+        <div className={s.cartitem}>
+          <img src="https://blocks.astratic.com/img/general-img-landscape.png" alt="image placeholder" />
+          <div className={s.cartinfo}>
+            <h2>Item name</h2>
+            <span className={s.line}></span>
+            <p>Price: $434</p>
+            <p>Discount price here</p>
+            <div className={s.quantitydisplay}>
+              <button className={s.quantitybutton}>-</button><span>1</span><button className={s.quantitybutton}>+</button>
+            </div>
+            <p>Subtotal:</p>
+            <button className={s.removeproduct}>Remove product</button>
+          </div>
+          <div className={s.ordersummary}>
+            <h2>Order Summary</h2>
+            <span className={s.line}></span>
+            <p>Cart total: $87</p>
+            <div className={s.cartoptionbuttons}>
+              <button className={s.proceedbtn}>Proceed to checkout</button>
+              <button className={s.clearcartbtn}>Clear cart</button>
+            </div>
+          </div>
         </div>
       </div>
-      <div className={s.cartbuttons}><button>Continue to shop</button>       <button>Proceed to checkout</button></div>
+      <div className={s.cartbuttonwrapper}>
+        <div className={s.cartbuttons}>
+          <button>Continue to shop</button>
+        </div>
+      </div>
     </section>
 
   )
