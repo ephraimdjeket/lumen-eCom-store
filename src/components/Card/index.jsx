f6447a0248a29b12837d510638f9c2d10f02aead
Ephraim Djeket <ephdje50146@stud.noroff.no>
2025-10-09 20:15:01 +0200

    Added conditional rendering for discountprice

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,15 +1,31 @@
 import c from "./Card.module.css";
+import { Link } from "react-router-dom";
 
-export default function Card({ title, price, image, rating }) {
+export default function Card({ title, price, discountedPrice, image, rating, id }) {
+  const url = "https://v2.api.noroff.dev/online-shop";
 
   return (
     <article className={c.cardstyle}>
       <img className={c.image} src={image.url} alt={image.alt} />
       <h2>{title}</h2>
-      <p>${price}</p>
+      {price === discountedPrice ? (
+        <div className={c.pricecontainer}>
+          <h2 className={c.price}>${price}</h2>
+        </div>
+      ) : (
+        <div className={c.pricescontainer}>
+          <h3 className={c.price}> ${discountedPrice}</h3>
+          <div className={c.pricecontainer}>
+            <div className={c.realpricecontainer}>
+              <p className={c.realprice}>${price}</p>
+              <span>Save {(Math.round(price - discountedPrice / price) / 100)}%</span></div>
+
+          </div>
+        </div>
+      )}
       {rating ? <span> Rating: {rating}</span> : "No rating"}
 
-      <a className={c.viewproduct}>View Product</a>
+      <Link to={`/productdetail/${id}`} className={c.viewproduct}>View Product</Link>
     </article >
   )
 }
