163af63f1898c46d948d55b2c1f9121ed2e5ab09
Ephraim Djeket <ephdje50146@stud.noroff.no>
2025-10-12 20:15:42 +0200

    Removed social media icons

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,17 +1,9 @@
 import f from "./Footer.module.css";
-import { SiFacebook, SiInstagram, SiX } from "react-icons/si";
 
 export default function Footer() {
   return (
     <footer className={f.footer}>
       <div className={f.footerContent}>
-        <ul className={f.list}>
-          <li><a href="#">Terms of Services</a></li>
-          <li><a href="#"><SiFacebook size={20} /></a></li>
-          <li><a href="#"><SiInstagram size={20} /></a></li>
-          <li><a href="#"><SiX size={20} /></a></li>
-
-        </ul>
         <p>© 2025 Ephraim Djeket</p>
       </div>
     </footer>
