2027714466bda7923cdf2741765d5315a5bd94f9
Ephraim Djeket <ephdje50146@stud.noroff.no>
2025-10-12 20:12:16 +0200

    Added Strictmode and cartprovider component

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,19 @@
-import { BrowserRouter } from 'react-router-dom';
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
 import "./styles/tokens.css";
 import "./styles/global.css";
-import App from './App.jsx'
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-)
+import { CartProvider } from "./cart/cartProvider.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <CartProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </CartProvider>
+  </StrictMode>
+);
