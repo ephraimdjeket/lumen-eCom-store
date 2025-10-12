45b5a0aa40c0a6d3d0522edf350024d1552adf0a
Ephraim Djeket <ephdje50146@stud.noroff.no>
2025-10-12 21:27:55 +0200

    Add cartProvider.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { CartProvider } from "../src/cart/cartProvider.jsx";
+import { CartProvider } from "./cart/CartProvider.jsx";
 import "./styles/tokens.css";
 import "./styles/global.css";
 import App from "./App.jsx";
